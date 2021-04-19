ad8445ee6f8b10ce17ca4cfb89fca979cb21a7fd
tanben2012 <tanben2012@163.com>
2021-04-19 15:30:31 +0800

    静态页面完成

---
+++ store/home.js
@@ -0,0 +1,10 @@
+export const state = () => ({ menu: [], hotPlace: [] })
+
+export const mutations = {
+  setMenu (state, val) {
+    state.menu = val
+  },
+  setHotPlace (state, val) {
+    state.hotPlace = val
+  }
+}
